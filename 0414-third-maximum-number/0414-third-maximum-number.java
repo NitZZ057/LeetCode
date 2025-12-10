321a8498ea5e709bdaac1de2605f07a3f89565f6
Nitesh Jadhav <126573647+NitZZ057@users.noreply.github.com>
2025-12-10 23:29:41 +0530

    Time: 5 ms (44.52%), Space: 46.8 MB (7.54%) - LeetHub

--- 0414-third-maximum-number/0414-third-maximum-number.java
+++ 0414-third-maximum-number/0414-third-maximum-number.java
@@ -1,30 +1,41 @@
-import java.util.*;
-
-public class Solution {
+class Solution {
     public int thirdMax(int[] nums) {
-        TreeSet<Integer> set = new TreeSet<>(Collections.reverseOrder());
-
-        for (int num : nums) {
-            set.add(num);
-        }
-
-        if (set.size() < 3) {
-            return set.first();
-        }
-
-        Iterator<Integer> iterator = set.iterator();
-        int count = 0;
-        int thirdMax = 0;
+        // Initialize three variables for the top 3 distinct max values
+        long a = Long.MIN_VALUE, b = Long.MIN_VALUE, c = Long.MIN_VALUE;
         
-        // Iterate to the third largest element
-        while (iterator.hasNext()) {
-            thirdMax = iterator.next();
-            count++;
-            if (count == 3) {
-                break;
+        // Use a set to keep track of distinct values
+        Set<Long> seen = new HashSet<>();
+        
+        // Iterate over the array
+        for (int num : nums) {
+            // Convert to long to handle Integer.MIN_VALUE properly
+            long value = (long) num;
+            
+            // Skip if the number has already been seen
+            if (seen.contains(value)) continue;
+            
+            // Add the number to the set of seen numbers
+            seen.add(value);
+            
+            // Update the top 3 max values
+            if (value > a) {
+                c = b;
+                b = a;
+                a = value;
+            } else if (value > b) {
+                c = b;
+                b = value;
+            } else if (value > c) {
+                c = value;
             }
         }
-
-        return thirdMax;
+        
+        // If we found at least 3 distinct numbers, return the third largest
+        if (seen.size() >= 3) {
+            return (int) c;
+        }
+        
+        // Otherwise, return the largest number
+        return (int) a;
     }
 }
