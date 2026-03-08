2d1e8c5b30890d955f6230ca5dd8d2cef102cf04
Nitesh Jadhav <126573647+NitZZ057@users.noreply.github.com>
2026-03-08 22:22:31 +0530

    Time: 13 ms (11.52%), Space: 52 MB (18.14%) - LeetHub

--- 0036-valid-sudoku/0036-valid-sudoku.java
+++ 0036-valid-sudoku/0036-valid-sudoku.java
@@ -4,8 +4,8 @@ class Solution {
 
         for(int i = 0; i < board.length; i++){
             for(int j = 0; j < board[0].length; j++){
-                if(board[i][j] != '.'){
-                    if(!set.add(i+"R"+board[i][j]) || !set.add(j+"C"+board[i][j]) || !set.add(i/3+"R"+j/3+"C"+board[i][j])){
+                if(board[i][j]!='.'){
+                    if(!set.add(i+"R"+board[i][j]) || !set.add(j+"C"+board[i][j]) || !set.add(i/3+"R"+board[i][j]+j/3+"C"+board[i][j])){
                         return false;
                     }
                 }
